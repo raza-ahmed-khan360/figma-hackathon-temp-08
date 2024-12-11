a95d2ac62a2419596959e0e8b561b46c1a015aa2
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2024-12-11 22:21:23 +0500

    Categories.tsx

--- app/home/sections/Categories.tsx
+++ app/home/sections/Categories.tsx
@@ -7,7 +7,7 @@ import CategoryProducts from '../../components/CardCategory';
 const TopCategotires:NextPage = () => {
   	return (
     		<div className="w-auto relative overflow-hidden flex flex-col items-center justify-center flex-wrap content-center gap-x-6 gap-y-10 text-left text-[32px] text-gray-scales-black font-inter">
-      			<div className="w-[1920px] flex flex-row items-center justify-start py-0 px-[300px] box-border">
+      			<div className="w-full container flex flex-row items-center justify-start">
         				<div className="relative leading-[110%] capitalize font-semibold">Top categories</div>
       			</div>
       			<CategoryProducts/>

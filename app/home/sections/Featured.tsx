0eb6aa869ce2690e17d59c057d11bf638ce25620
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2024-12-11 22:23:24 +0500

    Featured.tsx

--- app/home/sections/Featured.tsx
+++ app/home/sections/Featured.tsx
@@ -1,15 +1,18 @@
-import type { NextPage } from 'next';
-import CardFeatured from '../../components/CardFeatured';
+import type { NextPage } from "next";
+import CardFeatured from "../../components/CardFeatured";
 
 const FeaturedProducts: NextPage = () => {
   return (
-    <div className="w-auto h-[600px] flex flex-col items-center justify-center gap-[49px] overflow-hidden text-base text-gray-scales-black font-sale-price">
-      <div className="w-[1920px] flex flex-row items-center justify-start py-0 px-[300px] box-border">
-        <div className="relative text-[32px] leading-[110%] capitalize font-semibold">Featured Products</div>
+    <div className="flex flex-col items-center gap-12 px-4 py-10 text-gray-scales-black font-sale-price">
+      {/* Header */}
+      <div className="w-full container text-leftr">
+        <h2 className="text-2xl md:text-4xl font-semibold capitalize">
+          Featured Products
+        </h2>
       </div>
 
       {/* Flexbox container for cards */}
-      <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+      <div className="flex flex-wrap justify-center gap-8">
         <CardFeatured
           imageSrc="/homepage/Featured/Image.png"
           title="Library Stool Chair"

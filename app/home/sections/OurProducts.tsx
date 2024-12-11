e142e84164cb18ef55f0130f4e6ccbc11b66c09d
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2024-12-11 23:23:19 +0500

    OurProducts.tsx

--- app/home/sections/OurProducts.tsx
+++ app/home/sections/OurProducts.tsx
@@ -6,7 +6,7 @@ import CardGrid from '../../components/CardProducts';
 
 const OurProducts:NextPage = () => {
   	return (
-        <div className="w-full h-[1200px] overflow-hidden text-left text-[32px] text-gray-scales-black font-sale-price flex flex-col items-center">
+        <div className="w-full overflow-hidden text-left text-[32px] text-gray-scales-black font-sale-price flex flex-col items-center">
         <h2 className="text-2xl md:text-3xl font-semibold leading-[110%] capitalize my-4">Our Products</h2>
         <div className="w-full max-w-3xl h-8 mb-6"></div>
         <CardGrid />

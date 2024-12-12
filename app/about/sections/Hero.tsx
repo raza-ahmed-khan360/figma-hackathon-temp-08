ebf40bb70747958a177c0f19b077c66221769225
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2024-12-12 23:12:35 +0500

    Hero.tsx

--- app/about/sections/Hero.tsx
+++ app/about/sections/Hero.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 
 const AboutHero:NextPage = () => {
   	return (
